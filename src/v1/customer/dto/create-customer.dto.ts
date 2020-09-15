5ce6f08f47bdebbe043d0a0be9c34966e51f2a2a
Murilo Bezerril <murilo.bezerril@gmail.com>
2020-09-15 16:23:11 -0300

    chore: add git msg lint

--- src/v1/customer/dto/create-customer.dto.ts
+++ src/v1/customer/dto/create-customer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCustomerDto {}
+export class CreateCustomerDto {
+    name: string;
+    cpf?: string;
+    data_nascimento?: Date;
+    sexo?: string;
+    razao_social?: string;
+    nome_fantasia?: string;
+    cnpj?: string;
+    ie?: string;
+    is_isento?: boolean;
+    email: string;
+    telefone_fisico?: string;
+    telefone_movel: string;
+}
